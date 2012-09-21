a11500a92e0adb2cd5a69dbfd6fd8deeab9f0998
Péter Gombos <gombos@stud.ntnu.no>
2012-09-21 09:16:40 +0200

    fixed astar

--- exercise2/astar.py
+++ exercise2/astar.py
@@ -8,23 +8,32 @@ class searchNode:
 
 
 def aStarSearch(startNode):
+    closedSet = []
     agenda = [startNode]
+    cameFrom = {}
     startNode.g = 0
     startNode.f = startNode.h + startNode.g
 
     for node in agenda:
         if node.goal:
             return node
+        closedSet.append(agenda.pop(0))
         for child in node.children:
-            if child != node:
+            if child not in closedSet:
+                cameFrom[child] = node
                 child.opened = True
                 child.f = node.g+getDistance() + child.h
                 child.parent = node
                 agenda.append(child)
-        agenda.pop(0)
         agenda.sort(key=attrget('f'))
 
+p = []
+def reconstructPath(cameFrom, currentNode):
+    if cameFrom[currentNode]:
+        p.append(reconstructPath(cameFrom, cameFrom[currentNode])
+        return [currentNode].append(p)
+    else:
+        return currentNode
 
-
-def getDistance():
+def getHeuristicEstimate(node):
     return 1
