2b55875886afd2995cd9c99239a0d7050907df3c
Péter Henrik Gombos <gombos@pvv.ntnu.no>
2012-10-09 12:03:05 +0200

    added general algorithm for simmulated annealing

--- exercise3/sa.py
+++ exercise3/sa.py
@@ -1,5 +1,31 @@
+import math
+import random
 
 class SearchNode:
     def __init__(self, state):
         self.state = state
 
+    def generate_neighbours(self, F):
+        pass
+
+    def get_random_neighbour(self)
+        pass
+
+
+def sa(start_node, F, F_target, T_max, dT):
+    P = start_node
+    T = T_max
+    while True:
+        if F(P) >= F_target:
+            #the current node is a good solution
+            return P
+        
+        P_max = P.generate_neighbours(F)
+        q = (F(P_max)-F(P))/F(P)
+        p = min(1, math.exp((-q)/T))
+        x = random.random()
+        if x>p:
+            P = P_max
+        else:
+            P = P.get_random_neighbour()
+        T = T - dT
